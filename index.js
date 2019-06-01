c329d8144cb41332527761c3bf21d4f33f71507d
Jeppe <jensemann92@gmail.com>
2019-06-01 14:39:46 +0200

    Kjør

---
+++ index.js
@@ -0,0 +1,26 @@
+
+  $( function() {
+    $( "#sortable1, #sortable2, #sortable3" ).sortable({
+      connectWith: ".connectedSortable"
+    }).disableSelection();
+  } );
+
+function addTask1(){
+    let task = document.createElement("li");
+    task.classList.add("boks");
+    document.getElementById("sortable1").appendChild(task);
+    
+}
+
+function addTask2(){
+    let task = document.createElement("li");
+    task.classList.add("boks2");
+    document.getElementById("sortable2").appendChild(task);
+    
+}
+function addTask3(){
+    let task = document.createElement("li");
+    task.classList.add("boks3");
+    document.getElementById("sortable3").appendChild(task);
+    
+}
