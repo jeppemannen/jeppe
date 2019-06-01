6c4258a5c6cb21a8015b8e5785e5b5786dfde5f0
Jeppe <jensemann92@gmail.com>
2019-06-01 17:19:15 +0200

    1.juni 17:20

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
 
   $( function() {
-    $( "#sortable1, #sortable2, #sortable3" ).sortable({
-      connectWith: ".connectedSortable"
+    $( "#table1, #table2, #table3").sortable({
+      connectWith: ".123"
     }).disableSelection();
   } );
 
 function addTask1(){
     let task = document.createElement("li");
     task.classList.add("boks");
-    document.getElementById("sortable1").appendChild(task);
+    document.getElementById("table1").appendChild(task);
+
+    
     
 }
 
 function addTask2(){
     let task = document.createElement("li");
     task.classList.add("boks2");
-    document.getElementById("sortable2").appendChild(task);
+    document.getElementById("table2").appendChild(task);
     
 }
 function addTask3(){
     let task = document.createElement("li");
     task.classList.add("boks3");
-    document.getElementById("sortable3").appendChild(task);
+    document.getElementById("table3").appendChild(task);
     
 }
+
